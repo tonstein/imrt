91e82235cd1e5d24c27b653ad52d180398b1238c
tonstein <tom.steinle@web.de>
2025-01-28 14:20:35 +0100

    use imgui math operators for derived color

--- examples/utility/src/style.h
+++ examples/utility/src/style.h
@@ -1,33 +1,37 @@
 #pragma once
 
+#include "imgui.h"
 #include <imrt.h>
 
-// clang-format off
 namespace ImRt {
 namespace Color {
 
-constexpr ImVec4 White100   = ImColor(212, 212, 212).Value;
-constexpr ImVec4 Cyan100    = ImColor(141, 200, 255).Value;
-constexpr ImVec4 Magenta100 = ImColor(255, 155, 203).Value;
-constexpr ImVec4 Yellow100  = ImColor(255, 206, 87).Value;
-constexpr ImVec4 Green100   = ImColor(117, 196, 121).Value;
+   static const ImVec4 Black   = ImColor(17, 17, 17).Value;
+   static const ImVec4 White   = ImColor(212, 212, 212).Value;
+   static const ImVec4 Blue    = ImColor(141, 200, 255).Value;
+   static const ImVec4 Magenta = ImColor(255, 155, 203).Value;
+   static const ImVec4 Yellow  = ImColor(255, 206, 87).Value;
+   static const ImVec4 Green   = ImColor(117, 196, 121).Value;
 
-// Derived colors
-constexpr ImVec4 Black      = ImVec4(White100.x * 0.08f, White100.y * 0.08f, White100.z * 0.08f, 1.0f);
-constexpr ImVec4 Gray12     = ImVec4(White100.x * 0.12f, White100.y * 0.12f, White100.z * 0.12f, 1.0f);
-constexpr ImVec4 Gray16     = ImVec4(White100.x * 0.16f, White100.y * 0.16f, White100.z * 0.16f, 1.0f);
-constexpr ImVec4 Gray19     = ImVec4(White100.x * 0.19f, White100.y * 0.19f, White100.z * 0.19f, 1.0f);
-constexpr ImVec4 Cyan10     = ImVec4(Cyan100.x  * 0.1f,  Cyan100.y  * 0.1f,  Cyan100.z  * 0.1f,  1.0f);
-constexpr ImVec4 MagentaA30 = ImVec4(Magenta100.x, Magenta100.y, Magenta100.z, 0.3f);
-constexpr ImVec4 CyanA30    = ImVec4(Cyan100.x, Cyan100.y, Cyan100.z, 0.3f);
+   // Scalars
+   static const ImVec4 Dark        = ImVec4(0.12f, 0.12f, 0.12f, 1.0f);
+   static const ImVec4 Light       = ImVec4(1.45f, 1.45f, 1.45f, 1.0f);
+   static const ImVec4 Transparent = ImVec4(1.0f, 1.0f, 1.0f, 0.3f);
+
+   // Derived Colors [FixMe] Make sure not to scale to a value > 1.
+   static const ImVec4 DarkGray           = Black * Light;
+   static const ImVec4 Gray               = DarkGray * Light;
+   static const ImVec4 DarkBlue           = Blue * Dark;
+   static const ImVec4 TransparentMagenta = Magenta * Transparent;
+   static const ImVec4 TransparentCyan    = Blue * Transparent;
 
 } // namespace Color
-// clang-format on
 
 static ImRt::Style DefaultStyle()
 {
    ImRt::Style style;
 
+   // ImGui style
    style.gui.WindowBorderSize       = 0;
    style.gui.GrabMinSize            = 40;
    style.gui.ItemInnerSpacing       = { 8, 8 };
@@ -43,30 +47,33 @@ static ImRt::Style DefaultStyle()
    style.gui.AntiAliasedLinesUseTex = true;
    style.gui.AntiAliasedFill        = true;
 
-   style.gui.Colors[ImGuiCol_Text]             = ImRt::Color::White100;
-   style.gui.Colors[ImGuiCol_WindowBg]         = ImRt::Color::Gray12;
-   style.gui.Colors[ImGuiCol_TitleBg]          = ImRt::Color::Gray16;
-   style.gui.Colors[ImGuiCol_TitleBgCollapsed] = ImRt::Color::Gray16;
-   style.gui.Colors[ImGuiCol_TitleBgActive]    = ImRt::Color::Black;
-   style.gui.Colors[ImGuiCol_ResizeGrip]       = ImRt::Color::Gray16;
-   style.gui.Colors[ImGuiCol_FrameBg]          = ImRt::Color::Black;
-   style.gui.Colors[ImGuiCol_FrameBgHovered]   = ImRt::Color::Black;
-   style.gui.Colors[ImGuiCol_FrameBgActive]    = ImRt::Color::Black;
-   style.gui.Colors[ImGuiCol_SliderGrab]       = ImRt::Color::Gray16;
-   style.gui.Colors[ImGuiCol_SliderGrabActive] = ImRt::Color::Gray19;
-   style.gui.Colors[ImGuiCol_Button]           = ImRt::Color::White100;
-   style.gui.Colors[ImGuiCol_ButtonHovered]    = ImRt::Color::White100;
-   style.gui.Colors[ImGuiCol_ButtonActive]     = ImRt::Color::White100;
-   style.gui.Colors[ImGuiCol_CheckMark]        = ImRt::Color::Green100;
+   ImVec4* colors = style.gui.Colors;
 
+   colors[ImGuiCol_Text]             = ImRt::Color::White;
+   colors[ImGuiCol_WindowBg]         = ImRt::Color::DarkGray;
+   colors[ImGuiCol_FrameBg]          = ImRt::Color::Black;
+   colors[ImGuiCol_FrameBgHovered]   = ImRt::Color::Black;
+   colors[ImGuiCol_FrameBgActive]    = ImRt::Color::Black;
+   colors[ImGuiCol_SliderGrab]       = ImRt::Color::Gray;
+   colors[ImGuiCol_SliderGrabActive] = ImRt::Color::Gray;
+   colors[ImGuiCol_CheckMark]        = ImRt::Color::Green;
+   colors[ImGuiCol_Button]           = ImRt::Color::White;
+   colors[ImGuiCol_ButtonHovered]    = ImRt::Color::White;
+   colors[ImGuiCol_ButtonActive]     = ImRt::Color::White;
+   colors[ImGuiCol_CheckMark]        = ImRt::Color::White;
+
+   // ImPlot style
    style.plot.PlotBorderSize = 0;
 
-   style.plot.Colors[ImPlotCol_Line]       = ImRt::Color::Magenta100;
-   style.plot.Colors[ImPlotCol_Fill]       = ImRt::Color::MagentaA30;
-   style.plot.Colors[ImPlotCol_PlotBg]     = ImRt::Color::Cyan10;
-   style.plot.Colors[ImPlotCol_Crosshairs] = ImRt::Color::Yellow100;
-   style.plot.Colors[ImPlotCol_InlayText]  = ImRt::Color::Yellow100;
+   colors = style.plot.Colors;
+
+   colors[ImPlotCol_Line]       = ImRt::Color::Magenta;
+   colors[ImPlotCol_Fill]       = ImRt::Color::TransparentMagenta;
+   colors[ImPlotCol_PlotBg]     = ImRt::Color::DarkBlue;
+   colors[ImPlotCol_Crosshairs] = ImRt::Color::Yellow;
+   colors[ImPlotCol_InlayText]  = ImRt::Color::Yellow;
 
    return style;
 }
+
 } // namespace ImRt
