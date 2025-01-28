673f8bef0ded171d568c9573788029df8b49c2fe
tonstein <tom.steinle@web.de>
2025-01-28 17:37:22 +0100

    add doxygen comments

--- src/imrt-params.h
+++ src/imrt-params.h
@@ -11,22 +11,59 @@
 
 namespace ImRt {
 
-/* ------------------------------------------------------ */
-/*                    parameter layout                    */
-/* ------------------------------------------------------ */
+/* -------------------------------------------------------------------------- */
+/*                    PARAMETER LAYOUT                                        */
+/* -------------------------------------------------------------------------- */
 
+/**
+ * @brief This class serves as a skeleton for defining a parameter. Note that a
+ * parameter layout does not have a current parameter value, but the derived
+ * classes GuiParameter and DspParameter have. These derived classes deal with
+ * the parameter value in different ways (cf. GuiParameter and DspParameter).
+ */
 class ParameterLayout
 {
 public:
+   /**
+    * @brief Construct a new parameter layout object that can be added to a
+    * collection of DSP parameters (cf. class DspParameters).
+    *
+    * @param id A unique ID used to identify the parameter e.g. when updating
+    * the GUI resp. DSP value of the parameter or when creating a GUI widget for
+    * the parameter.
+    * @param name The name displayed in the GUI widgets of the parameter.
+    * @param min The minimum value that the parameter can have.
+    * @param max The maximum value that the parameter can have.
+    * @param init The initial resp. default value for the parameter.
+    */
    ParameterLayout(
       uint32_t id, std::string name, float min, float max, float init
    );
    ParameterLayout() = delete;
 
+   /**
+    * @brief Returns the unique ID of the parameter.
+    */
    uint32_t id();
+
+   /**
+    * @brief Returns the name of the parameter.
+    */
    const char* name();
+
+   /**
+    * @brief Returns the minimum value of the parameter.
+    */
    float min();
+
+   /**
+    * @brief Returns the maximum value of the parameter.
+    */
    float max();
+
+   /**
+    * @brief Returns the initial resp. default value of the parameter.
+    */
    float init();
 
 protected:
@@ -35,9 +72,9 @@ protected:
    const float _min, _max, _init;
 };
 
-/* ------------------------------------------------------ */
-/*                      gui parameter                     */
-/* ------------------------------------------------------ */
+/* -------------------------------------------------------------------------- */
+/*                      GUI PARAMETER                                         */
+/* -------------------------------------------------------------------------- */
 
 struct GuiParameter : public ParameterLayout
 {
@@ -49,9 +86,9 @@ struct GuiParameter : public ParameterLayout
    float value;
 };
 
-/* ------------------------------------------------------ */
-/*                      dsp parameter                     */
-/* ------------------------------------------------------ */
+/* -------------------------------------------------------------------------- */
+/*                      DSP PARAMETER                                         */
+/* -------------------------------------------------------------------------- */
 
 class DspParameter : public ParameterLayout
 {
@@ -71,9 +108,9 @@ private:
    choc::fifo::VariableSizeFIFO _fifo;
 };
 
-/* ------------------------------------------------------ */
-/*                     gui parameters                     */
-/* ------------------------------------------------------ */
+/* -------------------------------------------------------------------------- */
+/*              COLLECTION OF GUI PARAMETERS                                  */
+/* -------------------------------------------------------------------------- */
 
 class DspParameters;
 
@@ -89,9 +126,9 @@ private:
    std::map<uint32_t, std::unique_ptr<GuiParameter>> _params;
 };
 
-/* ------------------------------------------------------ */
-/*                     dsp parameters                     */
-/* ------------------------------------------------------ */
+/* -------------------------------------------------------------------------- */
+/*              COLLECTION OF DSP PARAMETERS                                  */
+/* -------------------------------------------------------------------------- */
 
 class DspParameters
 {
