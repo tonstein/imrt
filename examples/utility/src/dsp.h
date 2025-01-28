e0c1adf9f49c50be0e86d3f32308b5307bebfacd
tonstein <tom.steinle@web.de>
2025-01-28 14:11:19 +0100

    correct typos

--- examples/utility/src/dsp.h
+++ examples/utility/src/dsp.h
@@ -7,7 +7,7 @@ class Dsp : public ImRt::Dsp<Dsp>
 public:
    Dsp(ImRt::DspSettings settings = ImRt::DspSettings());
 
-   int process(ImRt::Buffer& out, ImRt::Buffer& in, uint32_t numFrames);
+   int process(ImRt::Buffer& in, ImRt::Buffer& out, uint32_t numFrames);
 
 private:
    float gainValue, panValue, panAmountL, panAmountR;
